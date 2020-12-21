44c834c991bd47cac3dfb9ceac9c721abd821ca0
Mauricio Galvez <45831817+mau27galvez@users.noreply.github.com>
2020-12-21 10:56:14 -0500

    initial commit

---
+++ es/auth.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    // 'failed' => 'These credentials do not match our records.',
+    'failed' => 'Las credenciales no coinciden con nuestros datos.',
+    // 'password' => 'The provided password is incorrect.',
+    'password' => 'La contraseña es incorrecta.',
+    // 'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'throttle' => 'Ha intentado ingresar en demasiadas ocasiones. Inténtelo de nuevo en :seconds segundos.',
+
+];
---
+++ es/passwords.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    // 'reset' => 'Your password has been reset!',
+    'reset' => 'Su contraseña ha sido restablecida!',
+    // 'sent' => 'We have emailed your password reset link!',
+    'sent' => 'Le hemos enviado un correo con el enlace para restablecer su contraseña.',
+    // 'throttled' => 'Please wait before retrying.',
+    'throttled' => 'Por favor espere antes de volver a intentar.',
+    // 'token' => 'This password reset token is invalid.',
+    'token' => 'El token de restablecimiento de la contraseña no es valido.',
+    // 'user' => "We can't find a user with that email address.",
+    'user' => "No hemos podido encontrar algún usuario con ese correo.",
+
+];
